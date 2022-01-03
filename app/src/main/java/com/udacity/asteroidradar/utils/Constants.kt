6c479b4843542405f9dc22e7ff406a816b92b6b1
Marcel <plovv@users.noreply.github.com>
2022-01-04 01:09:02 +0200

    removed personal api key

--- app/src/main/java/com/udacity/asteroidradar/utils/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/utils/Constants.kt
@@ -4,5 +4,5 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "yyyy-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY = "7icdXmS7aM58yW7RVXqJs7PAVZhSEY1TjJ8iIfms"
+    const val API_KEY = "DEMO_KEY"
 }
